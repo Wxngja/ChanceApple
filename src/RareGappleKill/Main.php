fbe760925f68d46283e3f311e0c4a7c1544eaad8
BoyIsDino <arsenalimran@hotmail.com>
2016-05-17 20:08:30 +0100

    Create Main.php

---
+++ src/RareGappleKill/Main.php
@@ -0,0 +1,25 @@
+<?php
+namespace RareGappleKill;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\item\Item;
+use pocketmine\event\player\PlayerDeathEvent;
+class Main extends PluginBase Implements Listener{
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+$this->getLogger()->info("RareGappleKill v1.0.0 Enabled!");
+ }
+public function onDeath(PlayerDeathEvent $event){
+$entity = $event->getEntity();
+$killer = $entity->getLastDamageCause();
+$rand = mt_rand(1, 100);
+
+If($rand < 25){
+$killer->getInventory()->addItem(Item::get(ITEM::GOLDEN_APPLE,0,1));
+$killer->sendMessage(TF::GREEN . "[RGK] You have got a rare golden apple for killing " . $entity . " ! ");
+       }
+    }
+}
+
