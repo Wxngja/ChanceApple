683e815fa8bd1d2d6677ec69d990308e945ae200
BoyIsDino <arsenalimran@hotmail.com>
2016-05-17 20:26:51 +0100

    Update Main.php

--- src/RareGappleKill/Main.php
+++ src/RareGappleKill/Main.php
@@ -12,11 +12,11 @@ $this->getServer()->getPluginManager()->registerEvents($this,$this);
 $this->getLogger()->info("RareGappleKill v1.0.0 Enabled!");
  }
 public function onDeath(PlayerDeathEvent $event){
-$entity = $event->getEntity();
-$killer = $entity->getLastDamageCause();
+$player= $event->getPlayer();
+$killer = $player->getLastDamageCause();
 $rand = mt_rand(1, 100);
 
-If($rand < 25){
+If($rand => 25){
 $killer->getInventory()->addItem(Item::get(ITEM::GOLDEN_APPLE,0,1));
 $killer->sendMessage(TF::GREEN . "[RGK] You have got a rare golden apple for killing " . $entity . " ! ");
        }
