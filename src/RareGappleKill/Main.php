7db6f018f5a6d691984b02c1d1da3e7e96f5826b
JackNoordhuis <Jack@CrazedCraftMC.net>
2016-08-07 20:30:09 +1000

    Fix everything (hopefully)
    I've fixed everything I can see in the GitHub editor without testing it, make sure you use tabs instead of spaces and keep the same formatting through the whole file :p

--- src/RareGappleKill/Main.php
+++ src/RareGappleKill/Main.php
@@ -1,28 +1,35 @@
 namespace RareGappleKill;
-  
-  use pocketmine\event\Listener;
-  use pocketmine\event\player\PlayerDeathEvent;
-  use pocketmine\item\Item;
-  use pocketmine\plugin\PluginBase;
-  use pocketmine\utils\TextFormat as TF;
- class Main extends PluginBase Implements Listener
- {
- 	public function onEnable()
- 	{
- 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
- 		$this->getLogger()->info("RareGappleKill v1.0.0 Enabled!");
- 	}
-  
- 	public function onDeath(PlayerDeathEvent $event)
- 	{
- 		$entity = $event->getEntity();
- 		$killer = $entity->getLastDamageCause();
- 		$rand = mt_rand(1, 100);
- 		If ($rand => 25) {
- 		     If($killer instanceof Player){
- 			$killer->getInventory()->addItem(Item::get(ITEM::GOLDEN_APPLE, 0, 1));
- 			$killer->sendMessage(TF::GREEN . "[RGK] You have got a rare golden apple for killing " . $entity . " ! ");
- 		 }
- 		}
- 	}
- }
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase implements Listener
+{
+
+  public function onEnable()
+  {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info(TF::GREEN . "RareGappleKill v1.0.0 Enabled!");
+  }
+
+  public function onDeath(PlayerDeathEvent $event)
+  {
+    $entity = $event->getEntity();
+    $cause = $entity->getLastDamageCause();
+    if($cause instanceof EntityDamageByEntityEvent) {
+      $killer = $cause->getDamager();
+      if($killer instanceof Player) {
+        $rand = mt_rand(1, 4);
+        if($rand === 1) {
+          $killer->getInventory()->addItem(Item::get(Item::GOLDEN_APPLE));
+          $killer->sendMessage(TF::GREEN . "[RGK] You have received a rare golden apple for killing " . $entity->getDisplayName() . " ! ");
+        }
+      }
+    }
+  }
+
+}
